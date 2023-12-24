f18aee6a5bda2d9533e313973c547b122fd20bc1
Juan Giusti <jmgiusti91@gmail.com>
2023-12-24 18:15:29 -0300

    feat: not found exceptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -24,24 +24,19 @@ export class UsersService {
     const user = await this.repo.findOneBy({ id });
 
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
 
     Object.assign(user, attrs);
 
     return this.repo.save(user);
-
-    // return this.repo.save({
-    //   ...user,
-    //   ...attrs,
-    // });
   }
 
   async remove(id: number) {
     const user = await this.repo.findOneBy({ id });
 
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
 
     return this.repo.remove(user);
