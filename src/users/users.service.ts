8bfb2d920bb2b916c31cbc4a265ba0b109ca8737
Juan Giusti <jmgiusti91@gmail.com>
2023-12-24 15:28:23 -0300

    feat: user api finished

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,39 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  findByEmail(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+
+    // return this.repo.save({
+    //   ...user,
+    //   ...attrs,
+    // });
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.repo.remove(user);
+  }
 }
